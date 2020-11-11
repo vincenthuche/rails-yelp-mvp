1fef522cbd0e3138119de83d8c2c63288114a687
Vincent Huche <vincent@huche.net>
2020-11-11 14:58:13 +0100

    Yelp finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+brasserie = { name: "Brasserie", address: "Paris", category: "french" }
+sushi = { name: "Sushi", address: "Tokyo", category: "japanese" }
+friterie = { name: "Friterie", address: "Bruxelles", category: "belgian" }
+
+[dishoom, pizza_east, brasserie, sushi, friterie].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
